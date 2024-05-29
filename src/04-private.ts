b58150e9375341d40023d92460261e9e73cdb6a4
SoniaMEGS <megs.sonia@gmail.com>
2024-05-28 17:17:48 -0700

    public and private access

---
+++ src/04-private.ts
@@ -0,0 +1,49 @@
+//Podemos proteger nuestras propiedades y métodos para que nos sean accedidos y modificados por fuera de clase.
+
+//Private
+//Con esta palabra reservada private estamos restringiendo el acceso de nuestros parámetros y métodos, solo podrán ser accedidos o modificados dentro de la clase.
+
+export class MyDate {
+  year: number;
+  month: number;
+  private day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  printFormat(): string {
+    const day = this.addPadding(this.day);
+    const month = this.addPadding(this.month);
+    return `${day}/${month}/${this.year}`;
+  }
+
+  private addPadding(value: number) {
+    if (value < 10) {
+      return `0${value}`;
+    }
+    return `${value}`;
+  }
+
+  add(amount: number, type: 'days' | 'months' | 'years') {
+    if (type === 'days') {
+      this.day += amount;
+    }
+    if (type === 'months') {
+      this.month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
+
+  getDay() {
+    return this.day;
+  }
+}
+
+const myDate = new MyDate(1993, 7, 10);
+console.log(myDate.printFormat());
+console.log(myDate.getDay());
